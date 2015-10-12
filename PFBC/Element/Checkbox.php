adba5df1f7f99b038de7a84e23ae4502682e0191
Alexander Butenko <a.butenka@gmail.com>
2015-10-12 15:36:45 +0300

    fix checkbox and radio rendering

--- PFBC/Element/Checkbox.php
+++ PFBC/Element/Checkbox.php
@@ -14,19 +14,24 @@ class Element_Checkbox extends OptionElement {
 		if(substr($this->_attributes["name"], -2) != "[]")
 			$this->_attributes["name"] .= "[]";
 
-		$labelClass = $this->_attributes["type"];
+        $labelClass = $this->getAttribute ('class');
 		if(!empty($this->inline))
-			$labelClass .= " checkbox-inline";
+			$labelClass .= "checkbox-inline";
 
 		$count = 0;
+        echo '<div class="checkbox">';
 		foreach($this->options as $value => $text) {
 			$value = $this->getOptionValue($value);
 
-			echo '<label class="', $labelClass, '"> <input id="', $this->_attributes["id"], '-', $count, '"', $this->getAttributes(array("id", "value", "checked", "required")), ' value="', $this->filter($value), '"';
+			echo '<label class="', $labelClass, '">';
+            echo '<input id="', $this->_attributes["id"], '-', $count, '"', $this->getAttributes(array("id", "class", "value", "checked", "required")), ' value="', $this->filter($value), '"';
 			if(in_array($value, $this->_attributes["value"]))
 				echo ' checked="checked"';
 			echo '/> ', $text, ' </label> ';
 			++$count;
-		}	
+            if ($labelClass != 'checkbox-inline')
+                echo '</div><div class="checkbox">';
+		}
+        echo '</div>';
 	}
 }
--- PFBC/Element/Radio.php
+++ PFBC/Element/Radio.php
@@ -4,19 +4,24 @@ class Element_Radio extends OptionElement {
 	protected $inline;
 
 	public function render() { 
-		$labelClass = $this->_attributes["type"];
+		$labelClass = $this->getAttribute ('class');
 		if(!empty($this->inline))
-			$labelClass .= " radio-inline";
+			$labelClass .= "radio-inline";
 
 		$count = 0;
+        echo '<div class="radio">';
 		foreach($this->options as $value => $text) {
 			$value = $this->getOptionValue($value);
 
-			echo '<label class="', $labelClass . '"> <input id="', $this->_attributes["id"], '-', $count, '"', $this->getAttributes(array("id", "value", "checked")), ' value="', $this->filter($value), '"';
+			echo '<label class="', $labelClass . '">';
+            echo '<input id="', $this->_attributes["id"], '-', $count, '"', $this->getAttributes(array("id", "class", "value", "checked")), ' value="', $this->filter($value), '"';
 			if(isset($this->_attributes["value"]) && $this->_attributes["value"] == $value)
 				echo ' checked="checked"';
 			echo '/> ', $text, ' </label> ';
 			++$count;
-		}	
+            if ($labelClass != 'radio-inline')
+                echo '</div><div class="radio">';
+		}
+        echo '</div>';
 	}
 }
