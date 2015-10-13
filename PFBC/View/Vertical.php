4b2a6dd2c4d928ca1ba205197af1eec481ba5c20
Alexander Butenko <a.butenka@gmail.com>
2015-10-13 15:44:22 +0300

    Better rendering for Vertical view

--- PFBC/View/Vertical.php
+++ PFBC/View/Vertical.php
@@ -1,5 +1,6 @@
 <?php
 class View_Vertical extends FormView {
+    private $sharedCount = 0;
     public function renderElement ($element) {
 		if ($element instanceof Element_Hidden || $element instanceof Element_HTML || $element instanceof Element_Button) {
 			$element->render();
@@ -8,17 +9,28 @@ class View_Vertical extends FormView {
 		if (!$element instanceof Element_Radio && !$element instanceof Element_Checkbox && !$element instanceof Element_File)
 			$element->appendAttribute("class", "form-control");
 
+        if (!$element->getAttribute("shared") || $this->sharedCount == 0) {
+            $rowClass = $element->getAttribute ("shared") ? 'row' : '';
+            echo '<div class="'.$rowClass.' form-group elem-'.$element->getAttribute("id").'"> ', $this->renderLabel($element);
+        }
+
+        if ($element->getAttribute ("shared")) {
+            $sharedSize = $element->getAttribute("shared");
+            $this->sharedCount += $sharedSize[strlen($sharedSize) - 1];
+            $colSize = $element->getAttribute ("shared");
+            echo " <div class='$colSize'> ";
+        }
+
         $element->setAttribute('placeholder', $element->getLabel());
 		echo $element->render(), $this->renderDescriptions($element);
+        if ($element->getAttribute ("shared"))
+            echo " </div> ";
+
+        if (!$element->getAttribute("shared") || $this->sharedCount == 12) {
+            $this->sharedCount = 0;
+            echo " </div> ";
+        }
     }
 
-	protected function renderLabel (Element $element) {
-		$label = $element->getLabel();
-		if(empty ($label))
-            $label = '';
-		echo '<label class="col-md-4 control-label" for="', $element->getAttribute("id"), '">';
-		if ($element->isRequired())
-			echo '<span class="required">* </span>';
-		echo '</label>';
-	}
+	protected function renderLabel (Element $element) {}
 }
