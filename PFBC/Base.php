c86e6ad4dd2da5fc4287efad3eaeccddd242aabf
Alexander Butenko <a.butenka@gmail.com>
2016-05-11 16:51:35 +0300

    issue #4 fix for php7

--- PFBC/Base.php
+++ PFBC/Base.php
@@ -24,10 +24,13 @@ abstract class Base {
                 if($property[0] != "_") {
                     /*If the appropriate class has a "set" method for the property provided, then
                     it is called instead or setting the property directly.*/
-                    if(isset($method_reference["set" . $property]))
-                        $this->$method_reference["set" . $property]($value);
-                    elseif(isset($property_reference[$property]))
-                        $this->$property_reference[$property] = $value;
+                    if(isset ($method_reference["set" . $property])) {
+                        $funcName = $method_reference["set" . $property];
+                        $this->$funcName ($value);
+                    } elseif (isset($property_reference[$property])) {
+                        $funcName = $property_reference[$property];
+                        $this->$funcName = $value;
+                    }
                     /*Entries that don't match an available class property are stored in the attributes
                     property if applicable.  Typically, these entries will be element attributes such as
                     class, value, onkeyup, etc.*/
